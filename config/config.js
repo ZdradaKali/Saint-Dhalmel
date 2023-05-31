9d6374b9f271b5620d9f2dbfbc15b3f271bdd4c8
Kali <41173208+H4tsuneM1ku@users.noreply.github.com>
2023-05-31 15:15:15 +0200

    Initial commit

--- config/config.js
+++ config/config.js
@@ -11,8 +11,8 @@ module.exports = {
   },
 
   Client: {
-    TOKEN: "MTA5NTc2NTAzMTE4Mzk5MDg1NA.GTIa9b.OdLbDREP1JH8cS0rqoVcSInvrf_uEzJiD_g3K4", // YOUR BOT TOKEN. (USE THIS ONLY IN VSCODE)
-    ID: "1095765031183990854" // YOUR BOT ID.
+    TOKEN: "", // YOUR BOT TOKEN. (USE THIS ONLY IN VSCODE)
+    ID: "" // YOUR BOT ID.
   }
 
 }
